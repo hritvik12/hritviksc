c4bbdf59e7809930cc405a95f6a81570015d6527
hritvik12 <49869591+hritvik12@users.noreply.github.com>
2019-04-22 20:55:53 +0530

    Update dnssfinal.sh

--- dnssfinal.sh
+++ dnssfinal.sh
@@ -4,7 +4,9 @@ echo " Welcome to dns configration script"
 
 yum install bind* -y
 
-wget https://github.com/hritvik12/hritviksc/blob/master/named.conf -P /etc/
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/hritviksc/master/named.conf'
+
+cp named.conf /etc/
 
 wget https://raw.githubusercontent.com/hritvik12/hritviksc/master/forward1 -P /var/named/
 
@@ -17,6 +19,10 @@ firewall-cmd --permanent --add-port=53/tcp
 firewall-cmd --permanent --add-port=53/udp
 
 firewall-cmd --reload
+
+rm /etc/hosts
+
+rm /etc/resolv.conf
 
 nslookup www.google.com
 echo "ban gyi script"
