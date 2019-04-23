c986e8f80e96fed818d4715920cde111d5be3db6
hritvik12 <49869591+hritvik12@users.noreply.github.com>
2019-04-23 15:54:38 +0530

    Add files via upload

---
+++ apache180.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo " Welcome to apache config "
+
+sudo yum install httpd* -y
+
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/hritviksc/master/1.conf'
+
+cp 1.conf /etc/httpd/conf.d/
+
+wget https://raw.githubusercontent.com/hritvik12/hritviksc/master/index.html 
+
+cp index.html /var/www/html/
+
+systemctl restart httpd
+
+curl www.google.com
+
